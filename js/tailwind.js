11361e9885fc450852d1c7a00555547474128e1a
dhanakbar <maulana261100@gmail.com>
2023-08-06 20:38:08 +0700

    Init

---
+++ js/tailwind.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "primary-100": "#F2F2F2",
+        "primary-200": "#DEDEDE",
+        "primary-300": "#CACACA",
+        "primary-400": "#B5B5B5",
+        "primary-500": "#A1A1A1",
+        "primary-600": "#8C8C8C",
+        "primary-700": "#787878",
+        "primary-800": "#646464",
+        "primary-900": "#4F4F4F",
+      },
+    },
+  },
+};
